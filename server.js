44b31ede1ae24051ea406b74293afa482d6dffc1
kalachuuchiiii <parissrowlet@gmail.com>
2025-05-27 15:50:49 +0800

    moved server.js to root

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require("dotenv").config(); 
 const express = require("express"); 
 const cors = require("cors");
-const { connectDb } = require("./database/index.js");
-const router = require("./router/router.js");
+const { connectDb } = require("./api/database/index.js");
+const router = require("./api/router/router.js");
 
 const app = express(); 
 const port = process.env.PORT
@@ -11,7 +11,7 @@ app.use(cors({
   origin: process.env.CLIENT_URL
 }))
 app.use("/api", router);
-
+//
 
 connectDb().then(() => {
   console.log("db connected")
